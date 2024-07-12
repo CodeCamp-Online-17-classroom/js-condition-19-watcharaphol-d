1b952719bddb55d53c7e5b1e0f6b03ebae4c7372
Watcharaphol D <dangpiem@hotmail.com>
2024-07-12 15:20:19 +0700

    Refractor grade calculation logic to use ternary

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+let score = +prompt("Please enter your score");
+let grade = (score >= 80) ? "A" :
+    (score >= 70) ? "B" :
+        (score >= 60) ? "C" :
+            (score >= 50) ? "D" :
+                (score >= 0) ? "F" : "Error";
+alert(`Your score is ${score} \n \t Get grade ${grade}`);
